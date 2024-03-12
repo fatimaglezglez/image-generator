c8a8bbe8f354c8d9ce90ed962269e4e3131b670d
Fátima González González <72091780+fatimaglezglez@users.noreply.github.com>
2024-03-12 17:22:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.markdown("# simple image generator :)")
 
 # API Token and model 
 
-REPLICATE_MODEL_ENDPOINTSTABILITY = "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4"
+REPLICATE_MODEL_ENDPOINTSTABILITY = "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b"
 #REPLICATE_MODEL_ENDPOINTSTABILITY = "stability-ai/sdxl:2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2"
 
 # Input prompt
