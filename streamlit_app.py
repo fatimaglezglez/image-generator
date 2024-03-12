457123b30e480b988196e7eca1dd5ab739466bad
Fátima González González <72091780+fatimaglezglez@users.noreply.github.com>
2024-03-12 16:41:47 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,6 @@ REPLICATE_MODEL_ENDPOINTSTABILITY = "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89
 #REPLICATE_MODEL_ENDPOINTSTABILITY = "stability-ai/sdxl:2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2"
 
 # Input prompt
- = st.text_input('What is your name?', '')
 prompt = st.text_input("Enter your prompt:", value="A beautiful landscape")
 prompt2 = prompt + style
 style = st.selectbox(
