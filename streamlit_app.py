c62a99f33ba1ad57eed9824f56513bd4b2172645
Fátima González González <72091780+fatimaglezglez@users.noreply.github.com>
2024-03-12 16:59:24 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ REPLICATE_MODEL_ENDPOINTSTABILITY = "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89
 prompt = st.text_input("enter your prompt:", value="a magical forest")
 style = st.selectbox(
     'pick a style',
-    ('art-nouveau', 'abstract art', 'photo')
+    ('art-nouveau', 'abstract art', 'tinted glass', 'photo')
 )
 prompt_with_style = f"{style} {prompt}"
 # Submit button
