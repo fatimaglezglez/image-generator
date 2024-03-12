a75dfb796c02764cdf868f43645252bb5a0bea74
Fátima González González <72091780+fatimaglezglez@users.noreply.github.com>
2024-03-12 15:52:01 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.markdown("# Simple Image Generator :))")
 REPLICATE_API_TOKEN = "r8_JAeYYpbpAiZkPNcdIUhJKuJnHQ3YiOQ3NW7hz"
 REPLICATE_MODEL_ENDPOINTSTABILITY = "stability-ai/sdxl:2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2"
 # Setup Replicate
-#replicate.api_token = REPLICATE_API_TOKEN
+replicate.api_token = REPLICATE_API_TOKEN
 
 # Input prompt
 
